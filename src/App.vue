d74296f72302d0a0cf2909cb2a5e160c6df5fe37
1480018180@qq.com <1480018180@qq.com>
2019-04-17 11:13:17 +0800

    Enter

---
+++ src/App.vue
@@ -0,0 +1,204 @@
+<template>
+  <div id="app">
+    <!-- 头 -->
+    <el-row style="background-color:#e0e0e0">
+      <el-col :span="2" style="background-color:#e0e0e0">
+        <div class="line"></div>
+          <img src="./assets/img/logo.png" alt="" style="height:55px;width:140px;background-color:#e0e0e0">
+      </el-col>
+      <el-col :span="20">
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#e0e0e0"
+          text-color="#000"
+          active-text-color="#008cd4"
+          active-background-color="#e0e0e0"
+        >
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">产品</template>
+            <el-menu-item index="2-1">
+              <el-submenu index="2-1">
+                <template slot="title">网站建设</template>
+                <el-menu-item index="2-1-1">品牌管网建设</el-menu-item>
+                <el-menu-item index="2-1-2">集团站群建设</el-menu-item>
+                <el-menu-item index="2-1-3">行业门户网站建设</el-menu-item>
+                <el-menu-item index="2-1-4">社区门户网站建设</el-menu-item>
+                <el-menu-item index="2-1-5">网站定制</el-menu-item>
+              </el-submenu>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <el-submenu index="2-2">
+                <template slot="title">移动端开发</template>
+                <el-menu-item index="2-2-1">手机网站建设</el-menu-item>
+                <el-menu-item index="2-2-2">微网站</el-menu-item>
+                <el-menu-item index="2-2-3">小程序开发</el-menu-item>
+                <el-menu-item index="2-2-4">App开发</el-menu-item>
+              </el-submenu>
+            </el-menu-item>
+            <el-menu-item index="2-3">
+              <el-submenu index="2-3">
+                <template slot="title">电商网站开发</template>
+                <el-menu-item index="2-3-1">B2B网站建设</el-menu-item>
+                <el-menu-item index="2-3-2">B2C网站建设</el-menu-item>
+                <el-menu-item index="2-3-3">为上策建设</el-menu-item>
+              </el-submenu>
+            </el-menu-item>
+            <el-menu-item index="2-4">SEO优化</el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">案例</template>
+            <el-menu-item index="3-1">制造业</el-menu-item>
+            <el-menu-item index="3-2">服务业</el-menu-item>
+            <el-menu-item index="3-3">餐饮业</el-menu-item>
+            <el-menu-item index="3-4">装饰业</el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">解决方案</template>
+            <el-menu-item index="4-1">电商行业</el-menu-item>
+            <el-menu-item index="4-2">制造业</el-menu-item>
+            <el-menu-item index="4-3">餐饮业</el-menu-item>
+            <el-menu-item index="4-4">装饰业</el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">关于我们</template>
+            <el-menu-item index="5-1">企业概况</el-menu-item>
+            <el-menu-item index="5-2">新闻中心</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="6">联系我们</el-menu-item>
+        </el-menu>
+      </el-col>
+    </el-row>
+    <router-view/>
+    <!-- footer -->
+    <el-row class="footer">
+      <!-- foot-left -->
+      <el-col :span="6">
+        <el-row>
+          <el-col :span="4" :offset="15">
+            <i class="el-icon-phone"></i>
+          </el-col>
+          <el-col :span="5" style="margin-top:0.6rem;text-align:left;">
+            售前资讯<p>xxx-xxx-xxx</p>
+          </el-col>
+        </el-row>
+      </el-col>
+      <!-- foot-conten -->
+      <el-col :span="9" :offset="2" class="foot_left_center">
+        <el-row>
+          <el-col :span="24" class="foot_left_center_top">
+            <a href="javascript:;">
+              联系我们
+            </a><span>|</span>
+            <a href="javascript:;">
+              意见反馈
+            </a><span>|</span>
+            <a href="javascript:;">
+              全国网点
+            </a><span>|</span>
+            <a href="javascript:;">
+              友情链接
+            </a><span>|</span>
+            <a href="javascript:;">
+              推荐
+            </a><span>|</span>
+            <a href="javascript:;">
+              邮箱登陆
+            </a><span>|</span>
+            <a href="javascript:;">
+              English
+            </a>
+          </el-col>
+          <el-col :span="24" class="foot_left_center_bottom">
+            版权所有 © 1999-2019 中企动力科技股份有限公司 Copyright © 1999-2018 300.cn All Rights Reserved京公网安备11030102010293号京ICP证010249-2
+          </el-col>
+        </el-row>
+      </el-col>
+      <!-- foot-right -->
+      <el-col :span="3">
+        <el-row>
+          <el-col :span="24">
+            <i class="el-icon-mobile-phone"></i>&nbsp;
+            <!-- <img src="./assets/img/vx.png" class="foot_right_Wx"> -->
+          </el-col>
+          <el-col :span="24" class="foot_right__leftText">
+            <span>移动版</span>&nbsp;&nbsp;&nbsp;
+            <!-- <span>微 信</span> -->
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+*{margin:0px;padding:0px;}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+/* 底部 */
+.footer{
+  margin-top:2rem;
+  padding:3rem;
+  background-color:rgba(0, 0, 0, 0.8);
+  color:#fff;
+}
+ .foot_right_Wx{
+  padding:0.6rem;
+  border:1px solid #fff;
+  border-radius: 50%;
+  font-size:12px;
+}
+.el-icon-mobile-phone{
+  padding:1rem;
+  border:1px solid #fff;
+  border-radius: 50%;
+  font-size:22px;
+}
+.el-icon-phone{
+  padding:1rem;
+  border:1px solid #fff;
+  border-radius: 50%;
+  font-size:32px;
+}
+.foot_left_center{
+  border-left:1px solid #fff;
+  border-right:1px solid #fff;
+}
+.foot_left_center a{
+  text-decoration: none;
+  color:#fff;
+}
+.foot_left_center a:hover{
+  color:#0ff;
+}
+.foot_left_center_bottom{
+  font-size:12px;
+  color:#c6c6c6;
+  margin-top:1rem;
+}
+.foot_right__leftText{
+  font-size:16px;
+}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,275 @@
+<template>
+  <div class="index">
+    <!-- 轮播图 -->
+    <template>
+      <el-carousel :interval="5000" arrow="never" indicator-position="outside">
+        <el-carousel-item v-for="item in banner" :key="item">
+          <img :src="item.idView" alt="">
+        </el-carousel-item>
+      </el-carousel>
+    </template>
+    <!-- 企业行业建站分类 -->
+    <el-row>
+      <el-col :span="24" class="site">
+        企业行业建站分类
+      </el-col>
+    </el-row>
+    <el-row type="flex" class="row-bg" justify="center">
+      <el-col :span="3">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>餐饮行业网站建设</span>
+          </div>
+          <div class="text item">
+            发展期企业，初创期企业，企业官网。以展示企业实力和品牌战略为主要方向。支持平台：手机、PC、APP、微信
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="3">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>培训行业网站建设</span>
+          </div>
+          <div class="text item">
+            发展期企业，初创期企业，企业官网。以展示企业实力和品牌战略为主要方向。支持平台：手机、PC、APP、微信
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="3">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>机械行业网站建设</span>
+          </div>
+          <div class="text item">
+            发展期企业，初创期企业，企业官网。以展示企业实力和品牌战略为主要方向。支持平台：手机、PC、APP、微信
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="3">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>家装行业网站建设</span>
+          </div>
+          <div class="text item">
+            发展期企业，初创期企业，企业官网。以展示企业实力和品牌战略为主要方向。支持平台：手机、PC、APP、微信
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="3">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>酒店民宿网站建设</span>
+          </div>
+          <div class="text item">
+            发展期企业，初创期企业，企业官网。以展示企业实力和品牌战略为主要方向。支持平台：手机、PC、APP、微信
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <!-- 行业解决方案 -->
+    <el-row>
+      <el-col :span="24" class="site">
+        行业解决方案
+      </el-col>
+    </el-row>
+    <el-row style="margin-top:2rem;">
+      <el-col :span="13" :offset="5">
+        <el-container>
+          <!-- 左边aside -->
+          <el-aside width="200px">
+            <el-row style="border:5px solid #666">
+              <el-col :span="15">
+                <el-button type="text">教育解决方案</el-button>
+              </el-col>
+              <el-col :span="15">
+                <el-button type="text">教育解决方案</el-button>
+              </el-col>
+              <el-col :span="15">
+                <el-button type="text">教育解决方案</el-button>
+              </el-col>
+              <el-col :span="15">
+                <el-button type="text">教育解决方案</el-button>
+              </el-col>
+            </el-row>
+          </el-aside>
+          <el-container>
+            <!-- 中间头部 -->
+            <el-header>新金融解决方案</el-header>
+            <!-- 中间主体 -->
+            <el-main>
+              <el-row>
+                <el-col :span="20">
+                  通过一系列云基础设施服务，帮助您创建、实施和分发全球数字媒体内容。具有较高的可扩展性和安全性，在云中为各种类型的视音频或数字内容提供海量存储容量，提供高效分发内容的低成本解决方案。
+                </el-col>
+                <el-col :span="5">
+                  <el-button type="danger" plain style="margin-top:2rem;">了解更多</el-button>
+                </el-col>
+              </el-row>
+            </el-main>
+            <!-- 中间底部 -->
+            <el-footer>
+              <el-row>
+                <el-col :span="6">
+                  <el-button type="danger">售后服务</el-button>
+                </el-col>
+                <el-col :span="6">
+                  <el-button type="danger">相关案例</el-button>
+                </el-col>
+                <el-col :span="6">
+                  <el-button type="danger">网站安全</el-button>
+                </el-col>
+              </el-row>
+            </el-footer>
+          </el-container>
+        </el-container>
+      </el-col>
+    </el-row>
+    <!-- 品牌企业案例 -->
+    <el-row>
+      <el-col :span="24" class="site">
+        品牌企业案例
+      </el-col>
+      <el-col :span="23">
+        <div class="pp_log">
+          <img src="../assets/img/logo.png" alt="">
+          <img src="../assets/img/logo.png" alt="">
+          <img src="../assets/img/logo.png" alt="">
+        </div>
+      </el-col>
+      <el-col :span="23">
+        <div class="pp_log">
+          <img src="../assets/img/logo.png" alt="">
+          <img src="../assets/img/logo.png" alt="">
+          <img src="../assets/img/logo.png" alt="">
+        </div>
+      </el-col>
+    </el-row>
+    <!-- 最新资讯 -->
+    <el-row>
+      <el-col :span="24" class="site">
+        最新资讯
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1',
+      banner: [
+        {id: 0, idView: require('../assets/img/banner1.jpg')}
+      ]
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* site(行业建站分类) */
+.site {
+  font-weight: 900;
+  margin-top: 1rem;
+  color:#666;
+  font-size:20px;
+}
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 205px;
+    margin-top:1rem;
+  }
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    padding-top: 2rem;
+    margin-bottom: 3rem;
+  }
+  /* 行业解决方案 */
+    .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: left;
+    margin-left: 2rem;
+    line-height: 60px;
+    font-weight: 900;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: left;
+    margin-top: 2rem;
+    margin-left: 2rem;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
+
+<style>
+  .index {
+    background-color:#e9e9e9;
+  }
+  .index .el-card {
+    background-color:#fff;
+  }
+  .index .clearfix{
+    font-weight:900;
+    color:#000;
+    background-color:rgba(255, 255, 255, 0.9)
+  }
+ .index .text{
+    background-color:rgba(0, 0, 0, 0);
+  }
+  .index .pp_log{
+    margin-top:1rem;
+    transition:.5s;
+  }
+  .index .pp_log img{
+    margin-left:1rem;
+    width:200px;
+    height:50px;
+    padding:1rem;
+    border:1px solid #c6c6c6;
+    background-color:rgba(0, 0, 0, 0.3);
+    transition:.5s
+  }
+  .index .pp_log img:hover{
+    border:1px solid #000;
+    background-color:rgba(0, 0, 0, 0)
+  }
+</style>
